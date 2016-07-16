3f07bc1f2b8b38bb2804c33ade22c21d83728fee
Neil <neilspurgeon@gmail.com>
2016-07-16 13:25:43 -0700

    refactored title function

--- js/app.js
+++ js/app.js
@@ -4,41 +4,31 @@ $(document).ready(function() {
   MenuTitleAnimation = (function() {
     console.log('MenuTitleAnimation triggered');
 
-    var didScroll = false;
-
     var triggerPos = window.innerHeight / 2;
-    var element = $('.project-trigger');
-    
-
-    window.onscroll = doThisStuffOnScroll;
 
     function doThisStuffOnScroll() {
-        didScroll = true;
+        console.log('do this stuff');
+
+        $('.project-trigger').each(function() {
+          var el    = $(this);
+          var elPos = el.offset().top;
+          var title = el.attr('data-title');
+          var charCount = title.split('').length;
+
+          if (window.scrollY >= (elPos - triggerPos)) {
+            // Change Title
+            $('#project-title')[0].innerHTML = title;
+            $('#project-title').css({width: (charCount / 2) + 'em', opacity: '2'});
+          } else if (window.scrollY <= triggerPos) {
+            // Remove Title
+            $('#project-title').css({width: '0', opacity: '0'});
+          }
+        });
     }
 
-    setInterval(function() {
-        if(didScroll) {
-            didScroll = false;
-            element.each(function() {
-              el    = $(this);
-              var elPos = el.offset().top;
-              var title = el.attr('data-title');
-              var charCount = title.split('').length;
+    doThisStuffOnScroll();
+    window.onscroll = doThisStuffOnScroll;
 
-              // pos = that;
-
-              if (window.scrollY >= (elPos - triggerPos)) {
-                $('#project-title')[0].innerHTML = title;
-                $('#project-title').css('width', (charCount / 2 + 'em'));
-                $('#project-title').css('opacity', '1');
-              } else if (window.scrollY <= (triggerPos )) {
-                // $('#project-title')[0].innerHTML = "";
-                $('#project-title').css('width', '0');
-                $('#project-title').css('opacity', '0');
-              }
-            });
-        }
-    }, 100);
   })();
 
 });
