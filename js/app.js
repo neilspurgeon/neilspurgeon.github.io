833073d7465bd5baae29f2d255bbe70a1db88ebc
Neil <neilspurgeon@gmail.com>
2016-07-23 10:18:33 -0700

    refactored scroll fn

--- js/app.js
+++ js/app.js
@@ -4,7 +4,41 @@ $.fn.textWidth = function(text, font) {
     return $.fn.textWidth.fakeEl.width();
 };
 
+$.fn.moveIt = function(){
+  var $window = $(window);
+  var instances = [];
+  
+  $(this).each(function(){
+    instances.push(new moveItItem($(this)));
+  });
+  
+  window.onscroll = function(){
+    var scrollTop = $window.scrollTop();
+    instances.forEach(function(inst){
+      inst.update(scrollTop);
+    });
+  };
+};
+
+// var moveItItem = function(el){
+//   this.el = $(el);
+//   this.speed = parseInt(this.el.attr('data-scroll-speed'));
+// };
+
+// moveItItem.prototype.update = function(scrollTop){
+//   var pos = scrollTop / this.speed;
+//   this.el.css('transform', 'translateY(' + -pos + 'px)');
+// };
+
+
+
+
+
 $(document).ready(function() {
+
+  // $(function(){
+  //   $('[data-scroll-speed]').moveIt();
+  // });
 
   var menuTitleAnimation = function () {
     var triggerPos = window.innerHeight / 2;
