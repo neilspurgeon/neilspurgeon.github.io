dc91233053ccb2958f98af0c1d8d913a91faa624
Neil <neilspurgeon@gmail.com>
2017-05-21 16:34:33 -0700

    animated menu button. Switched to full screen menu layout.

--- js/app.js
+++ js/app.js
@@ -43,33 +43,33 @@
 
 // var pageFunctions = function() {
 
-//   var menuBtn = document.getElementById('menu-btn');
-//   var menu = document.getElementById('primary-nav');
-//   var overlay = document.getElementById('menu-overlay');
-//   var body = document.body;
+  var menuBtn = document.getElementById('menu-btn');
+  var menu = document.getElementById('primary-nav');
+  var overlay = document.getElementById('menu-overlay');
+  var body = document.body;
 
-//   $('#menu-btn').on('click', function() {
-//     console.log('clicked');
-//     document.activeElement.blur();
-//     if (menu.classList.contains('is-open')) {
-//       menu.classList.remove('is-open');
-//       menuBtn.classList.remove('is-open');
-//       overlay.classList.remove('is-open');
-//       body.classList.remove('lock-scroll');
-//     } else {
-//       menu.classList.add('is-open');
-//       menuBtn.classList.add('is-open');
-//       overlay.classList.add('is-open');
-//       body.classList.add('lock-scroll');
-//     }
-//   });
+  $('#menu-btn').on('click', function() {
+    console.log('clicked');
+    document.activeElement.blur();
+    if (menu.classList.contains('is-open')) {
+      menu.classList.remove('is-open');
+      menuBtn.classList.remove('is-open');
+      overlay.classList.remove('is-open');
+      body.classList.remove('lock-scroll');
+    } else {
+      menu.classList.add('is-open');
+      menuBtn.classList.add('is-open');
+      overlay.classList.add('is-open');
+      body.classList.add('lock-scroll');
+    }
+  });
 
-//   overlay.onclick = function() {
-//     body.classList.remove('lock-scroll');
-//     menu.classList.remove('is-open');
-//     menuBtn.classList.remove('is-open');
-//     overlay.classList.remove('is-open');
-//   }
+  overlay.onclick = function() {
+    body.classList.remove('lock-scroll');
+    menu.classList.remove('is-open');
+    menuBtn.classList.remove('is-open');
+    overlay.classList.remove('is-open');
+  }
 
 //   // grab an element
 //   var el = document.getElementById('site-header');
