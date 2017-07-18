c4c5368d1845c18c005c51a108411930e7d1d3eb
Neil <neilspurgeon@gmail.com>
2017-07-17 18:34:11 -0700

    added babel. Removed jquery

--- js/app.js
+++ js/app.js
@@ -1,40 +1,5 @@
-$(function(){
-  'use strict';
-  var $page = $('#main'),
-      options = {
-        debug: true,
-        prefetch: false,
-        cacheLength: 2,
-        onStart: {
-          duration: 800, // Duration of our animation
-          render: function ($container) {
-            var menu = document.getElementById('primary-nav');
-            if (menu.classList.contains('is-open')) {
-              closeMenu();
-            }
-            // Add your CSS animation reversing class
-            $container.addClass('is-exiting');
-            // Restart your animation
-            smoothState.restartCSSAnimations();
-          }
-        },
-        onReady: {
-          duration: 0,
-          render: function ($container, $newContent) {
-            // Remove your CSS animation reversing class
-            $container.removeClass('is-exiting');
-            // Inject the new content
-            $container.html($newContent);
-            // Remove scroll lock caused by side nav
-            // $('body').removeClass('lock-scroll');
-          }
-        },
-        onAfter: function($container, $newContent) {
-          pageFunctions();
-        }
-      },
-      smoothState = $page.smoothState(options).data('smoothState');
-});
+---
+---
 
 function toggleMenu() {
   var menu = document.getElementById('primary-nav');
@@ -83,42 +48,15 @@ var pageFunctions = function() {
     var rellax = new Rellax('.rellax');
   }
 
-  $('#menu-btn').on('click', function() {
+  document.getElementById('menu-btn').onclick = function() {
     console.log('clicked');
     document.activeElement.blur();
     toggleMenu();
-  });
+  };
 
   document.getElementById('menu-overlay').onclick = function() {
     closeMenu();
   }
-
-  var reveal = function() {
-    var elements = document.getElementsByClassName('reveal');
-
-    for (i=0; i<elements.length; i++) {
-      if (elements[i].offsetTop < (window.scrollY + window.innerHeight + 10) ) {
-        elements[i].classList.remove('reveal--out');
-      }
-    }
-  }
-
-  window.setTimeout(function() {
-    reveal()
-  }, 00);
-
-  var didScroll = false;
-
-  window.onscroll = function() {
-    window.didScroll = true;
-  }
-
-  setInterval(function() {
-    if(didScroll) {
-      reveal();
-      didScroll = false;
-    }
-  });
 }
 
 pageFunctions();
