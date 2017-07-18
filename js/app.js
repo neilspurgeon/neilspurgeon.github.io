bf1ba4a4e4264d8c683df4f1ad90660b6fc2a0ab
Neil <neilspurgeon@gmail.com>
2017-07-17 22:16:28 -0700

    switched from smoothstate.js to barba.js

--- js/app.js
+++ js/app.js
@@ -34,29 +34,49 @@ function openMenu() {
   var footer = document.getElementById('site-footer');
   var body = document.body;
 
+
   menu.classList.add('is-open');
   menuBtn.classList.add('is-open');
   overlay.classList.add('menu-is-open');
   body.classList.add('lock-scroll');
   footer.classList.add('nav-is-open');
   menu.classList.add('scene-element--fadeinright');
+
+}
+
+function clickMenuLink() {
+  var menuLinks = document.querySelectorAll('.primary-nav__link');
+  var i;
+
+  for (i=0; i<menuLinks.length; i++) {
+    menuLinks[i].onclick = function() {
+      // console.log(menuLinks[i])
+      closeMenu();
+    }
+  }
+}
+
+clickMenuLink();
+
+document.getElementById('menu-btn').onclick = function() {
+  console.log('clicked');
+  document.activeElement.blur();
+  toggleMenu();
+};
+
+document.getElementById('menu-overlay').onclick = function() {
+  closeMenu();
 }
 
 var pageFunctions = function() {
-
-  if (document.querySelector(".rellax")) {
+  if (document.querySelector('.rellax')) {
     var rellax = new Rellax('.rellax');
-  }
-
-  document.getElementById('menu-btn').onclick = function() {
-    console.log('clicked');
-    document.activeElement.blur();
-    toggleMenu();
-  };
-
-  document.getElementById('menu-overlay').onclick = function() {
-    closeMenu();
   }
 }
 
+Barba.Pjax.start();
 pageFunctions();
+
+Barba.Dispatcher.on('newPageReady', function(currentStatus, oldStatus, container) {
+  pageFunctions();
+});
